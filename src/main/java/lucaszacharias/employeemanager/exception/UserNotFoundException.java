7b3d08aa11888ac7b62756f00847f713b874b0e3
lucaszacharias <lucaszacharias@hotmail.com>
2021-11-30 17:06:51 -0300

    first commit

---
+++ src/main/java/lucaszacharias/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package lucaszacharias.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lucaszacharias/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package lucaszacharias.employeemanager.service;
+
+import lucaszacharias.employeemanager.exception.UserNotFoundException;
+import lucaszacharias.employeemanager.model.Employee;
+import lucaszacharias.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("Usuario não encontrado por id: " + id) );
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
